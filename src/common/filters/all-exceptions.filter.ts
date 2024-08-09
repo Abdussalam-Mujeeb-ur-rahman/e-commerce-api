199621bcefe64dd1e394120c9563e35fb4f5cad7
Abdussalam-Mujeeb-ur-rahman <mjbabdussalam@gmail.com>
2024-08-09 01:58:12 +0100

    created an errorhandler using Exception Filter, and added it as a global errorhandler

---
+++ src/common/filters/all-exceptions.filter.ts
@@ -0,0 +1,31 @@
+import {
+    ExceptionFilter,
+    Catch,
+    ArgumentsHost,
+    HttpException,
+    HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+    catch(exception: unknown, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>();
+        const status = 
+          exception instanceof HttpException
+            ? exception.getStatus()
+            : HttpStatus.INTERNAL_SERVER_ERROR;
+        
+        const errorResponse = {
+            statusCode: status,
+            timestamp: new Date().toISOString(),
+            path: request.url,
+            message: exception instanceof HttpException ? exception.getResponse() : "Internal Server Error"
+        };
+
+        response.status(status).json(errorResponse);
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Register the filter globally
+  app.useGlobalFilters(new AllExceptionsFilter());
+
   await app.listen(3000);
 }
 bootstrap();
